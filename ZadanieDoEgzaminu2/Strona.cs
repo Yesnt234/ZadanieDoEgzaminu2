163b48739be869ef97a77ea5167d9f6c4d6bf2d4
Student <Student@TE-KAL-D-S14-03>
2026-02-02 11:56:00 +0100

    Implementacja klasy "Strona"

--- ZadanieDoEgzaminu2/Strona.cs
+++ ZadanieDoEgzaminu2/Strona.cs
@@ -8,5 +8,21 @@ namespace ZadanieDoEgzaminu2
 {
     internal class Strona
     {
+        public string trescStrony;
+        public uint numerStrony;
+        public uint licznikStron;
+
+        public Strona (string trescStrony, uint numerStrony)
+        {
+            this.trescStrony = trescStrony;
+            this.numerStrony = numerStrony;
+            licznikStron = numerStrony + 1;
+        }
+
+        public Strona(string trescStrony)
+        {
+            this.trescStrony = trescStrony;
+            this.numerStrony = licznikStron++;
+        }
     }
 }
