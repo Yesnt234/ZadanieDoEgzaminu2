8abfeb8f23c5a01932e02eafb33c505fbb11204b
Student <Student@TE-KAL-D-S14-03>
2026-02-02 12:03:55 +0100

    Implementacja klasy "Ksiazka"

--- ZadanieDoEgzaminu2/Ksiazka.cs
+++ ZadanieDoEgzaminu2/Ksiazka.cs
@@ -8,5 +8,17 @@ namespace ZadanieDoEgzaminu2
 {
     internal class Ksiazka
     {
+        public List<Strona> strony = new List<Strona>();
+        public Ksiazka (string tytul) 
+        { 
+            var strona = new Strona(tytul, 1);
+            strony.Add(strona);
+        }
+
+        public void DodajStrone(string trescStrony) 
+        { 
+            var strona = new Strona(trescStrony);
+            strony.Add(strona);
+        }
     }
 }
